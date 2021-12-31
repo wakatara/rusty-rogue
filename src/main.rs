67bec5bafa39d9caa9b617ef6810ba7733e54238
Daryl Manning <daryl@wakatara.com>
2021-12-30 19:24:15 -0800

    Added mapbuilder, tiles, and layers.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 mod map;
+mod map_builder;
 mod player;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
+    pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH /2;
+    pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT /2;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
     pub use crate::map::*;
+    pub use crate::map_builder::*;
     pub use crate::player::*;
 }
 
@@ -18,8 +22,10 @@ struct State {
 
 impl State {
     fn new() -> Self {
-        Self { 
-            map: Map::new(),
+        let mut rng = RandomNumberGenerator::new();
+        let map_builder = MapBuilder::new(&mut rng);
+        Self {
+            map: map_builder.map,
             player: Player::new(Point::new(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)) 
         }
     }
@@ -35,9 +41,15 @@ impl GameState for State {
 }
 
 fn main() -> BError {
-    let context = BTermBuilder::simple80x50()
+    let context = BTermBuilder::new()
         .with_title("Pukey Nephew Dungeon")
         .with_fps_cap(30.0)
+        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+        .with_tile_dimensions(32,32)
+        .with_resource_path("resources/")
+        .with_font("dungeonfont.png", 32, 32)
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .build()?;
 
     main_loop(context, State::new())
