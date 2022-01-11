4905ce466ef37284d826d9f10c7a7f65a912c03b
Daryl Manning <daryl@wakatara.com>
2022-01-10 17:11:40 -0800

    Added in heads up display for player.

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -7,6 +7,7 @@ mod collisions;
 mod random_move;
 mod movement;
 mod end_turn;
+mod hud;
 
 pub fn build_input_scheduler() -> Schedule {
     Schedule::builder()
@@ -14,6 +15,7 @@ pub fn build_input_scheduler() -> Schedule {
         .flush()
         //.add_system(collisions::collisions_system())
         .add_system(map_render::map_render_system())
+        .add_system(hud::hud_system())
         .add_system(entity_render::entity_render_system())
         // .add_system(random_move::random_move_system())
         // .add_system(end_turn::end_turn_system())
@@ -27,6 +29,7 @@ pub fn build_player_scheduler() -> Schedule {
         .add_system(collisions::collisions_system())
         .flush()
         .add_system(map_render::map_render_system())
+        .add_system(hud::hud_system())
         .add_system(entity_render::entity_render_system())
         .add_system(end_turn::end_turn_system())
         .build()
@@ -41,6 +44,7 @@ pub fn build_monster_scheduler() -> Schedule {
         .add_system(collisions::collisions_system())
         .flush()
         .add_system(map_render::map_render_system())
+        .add_system(hud::hud_system())
         .add_system(entity_render::entity_render_system())
         .add_system(end_turn::end_turn_system())
         .build()
