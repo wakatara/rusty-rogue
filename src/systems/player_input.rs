95449d3ebb1eba9ce2243eee4fca31ad958e5e50
Daryl Manning <daryl@wakatara.com>
2022-01-12 23:04:36 -0800

    Basic player and monster combat added.

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -9,8 +9,8 @@ pub fn player_input(
     #[resource] key: &Option<VirtualKeyCode>,
     #[resource] turn_state: &mut TurnState
 ) {
-    let mut players = <(Entity, &Point)>::query()
-        .filter(component::<Player>());
+    let mut players = <(Entity, &Point)>::query().filter(component::<Player>());
+
     if let Some(key) = *key {
         let delta = match key {
             VirtualKeyCode::Left => Point::new(-1, 0),
@@ -18,14 +18,41 @@ pub fn player_input(
             VirtualKeyCode::Up => Point::new(0, -1),
             VirtualKeyCode::Down => Point::new(0, 1),
             _ => Point::new(0, 0),
-            };
+        };
 
-        players.iter(ecs).for_each(| (entity, pos) | {
-            let destination = *pos + delta;
-            commands
-                .push(( (), WantsToMove{ entity: *entity, destination } ));
+        let (player_entity, destination) = players
+            .iter(ecs)
+            .find_map(|(entity, pos)| Some((*entity, *pos + delta)) )
+            .unwrap();
+        
+        let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
+
+        if delta.x !=0 || delta.y != 0 {
+            let mut hit_something = false;
+            enemies
+                .iter(ecs)
+                .filter(|(_, pos)| {
+                    **pos == destination
+                })
+            .for_each(|(entity, _) | {
+                hit_something = true;
+
+                commands
+                    .push(((), WantsToAttack{
+                        attacker: player_entity,
+                        victim: *entity,
+                    }));
         });
+
+        if !hit_something {
+            commands
+                .push(((), WantsToMove{
+                    entity: player_entity,
+                    destination
+                }));
+        }
         *turn_state = TurnState::PlayerTurn;
     }
+}
 }
 
