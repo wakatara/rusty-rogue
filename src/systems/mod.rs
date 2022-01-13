0748d21123d83f6488c8dd1ab947cbded4321fb5
Daryl Manning <daryl@wakatara.com>
2022-01-12 21:38:28 -0800

    Removed collision systems ahead of combat.

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -3,7 +3,6 @@ use crate::prelude::*;
 mod map_render;
 mod entity_render;
 mod player_input;
-mod collisions;
 mod random_move;
 mod movement;
 mod end_turn;
@@ -28,8 +27,6 @@ pub fn build_player_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(movement::movement_system())
         .flush()
-        .add_system(collisions::collisions_system())
-        .flush()
         .add_system(map_render::map_render_system())
         .add_system(hud::hud_system())
         .add_system(entity_render::entity_render_system())
@@ -42,8 +39,6 @@ pub fn build_monster_scheduler() -> Schedule {
         .add_system(random_move::random_move_system())
         .flush()
         .add_system(movement::movement_system())
-        .flush()
-        .add_system(collisions::collisions_system())
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(hud::hud_system())
