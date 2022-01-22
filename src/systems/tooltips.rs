b82d0776bc367c9580d0049a790b841349931a73
Daryl Manning <daryl@wakatara.com>
2022-01-22 15:30:04 +0800

    Hide tooltips as well so can't figure out monster locations.

--- src/systems/tooltips.rs
+++ src/systems/tooltips.rs
@@ -4,8 +4,17 @@ use crate::prelude::*;
 #[read_component(Point)]
 #[read_component(Name)]
 #[read_component(Health)]
+#[read_component(FieldOfView)]
+#[read_component(Player)]
 pub fn tooltips( ecs: &SubWorld, #[resource] mouse_pos: &Point, #[resource] camera: &Camera) {
     let mut positions = <(Entity, &Point, &Name)>::query();
+    let mut fov = <(&FieldOfView)>::query().filter(component::<Player>());
+
+    let player_fov = fov.iter(ecs).nth(0).unwrap();
+    positions
+        .iter(ecs)
+        .filter(|(_, pos, _)| **pos == map_pos && player_fov.visible_tiles.contains(&pos));
+        
 
     let offset = Point::new(camera.left_x, camera.top_y);
     let map_pos = *mouse_pos + offset;
