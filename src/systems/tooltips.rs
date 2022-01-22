ddd8f1a78fef8ea4b002236132a773c4f323d21c
Daryl Manning <daryl@wakatara.com>
2022-01-22 16:24:58 +0800

    Minor fix to tooltips hiding.

--- src/systems/tooltips.rs
+++ src/systems/tooltips.rs
@@ -8,16 +8,15 @@ use crate::prelude::*;
 #[read_component(Player)]
 pub fn tooltips( ecs: &SubWorld, #[resource] mouse_pos: &Point, #[resource] camera: &Camera) {
     let mut positions = <(Entity, &Point, &Name)>::query();
-    let mut fov = <(&FieldOfView)>::query().filter(component::<Player>());
+    let mut fov = <&FieldOfView>::query().filter(component::<Player>());
 
+    let offset = Point::new(camera.left_x, camera.top_y);
+    let map_pos = *mouse_pos + offset;
     let player_fov = fov.iter(ecs).nth(0).unwrap();
     positions
         .iter(ecs)
         .filter(|(_, pos, _)| **pos == map_pos && player_fov.visible_tiles.contains(&pos));
-        
 
-    let offset = Point::new(camera.left_x, camera.top_y);
-    let map_pos = *mouse_pos + offset;
     let mut draw_batch = DrawBatch::new();
     draw_batch.target(2);
     positions
