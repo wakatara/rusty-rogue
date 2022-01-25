99af8615f2561f638177dfbcf39f8d660931e884
Daryl Manning <daryl@wakatara.com>
2022-01-25 13:57:02 +0800

    [WIP] Map memory and darkening visited out-of-FOV areas.

--- src/map.rs
+++ src/map.rs
@@ -9,6 +9,7 @@ pub enum TileType {
 
 pub struct Map {
     pub tiles: Vec<TileType>,
+    pub revealed_tiles: Vec<bool>
 }
 
 pub fn map_idx(x: i32, y: i32) -> usize {
@@ -18,7 +19,8 @@ pub fn map_idx(x: i32, y: i32) -> usize {
 impl Map {
     pub fn new() -> Self {
         Self {
-            tiles: vec![TileType::Floor; NUM_TILES]
+            tiles: vec![TileType::Floor; NUM_TILES],
+            revealed_tiles: vec![false; NUM_TILES]
         }
     }
 
