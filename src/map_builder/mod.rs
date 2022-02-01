88cffba658fb4996a2588eedac5ad9249e93c40e
Daryl Manning <daryl@wakatara.com>
2022-02-01 17:31:04 +0800

    [WIP] Refactoring map builder for procedural - converted room builder.

--- src/map_builder/mod.rs
+++ src/map_builder/mod.rs
@@ -1,11 +1,12 @@
 use crate::prelude::*;
 const NUM_ROOMS: usize = 20;
-mod empty;
-use empty::EmptyArchitect;
+// mod empty;
+// use empty::EmptyArchitect;
+mod rooms;
+use rooms::RoomsArchitect;
 
 trait MapArchitect {
     fn new(&mut self, rng: &mut RandomNumberGenerator) -> MapBuilder;
-
 }
 
 pub struct MapBuilder {
@@ -18,39 +19,9 @@ pub struct MapBuilder {
 
 impl MapBuilder {
     pub fn new(rng: &mut RandomNumberGenerator) -> Self {
-        let mut architect = EmptyArchitect{};
+        let mut architect = RoomsArchitect{};
         architect.new(rng)
-    } 
-    // pub fn new(rng: &mut RandomNumberGenerator) -> Self {
-
-    //     let mut mb = MapBuilder{
-    //         map: Map::new(),
-    //         rooms: Vec::new(),
-    //         player_start: Point::zero(),
-    //         amulet_start: Point::zero()
-    //     };
-    //     mb.fill(TileType::Wall);
-    //     mb.build_random_rooms(rng);
-    //     mb.build_corridors(rng);
-    //     mb.player_start = mb.rooms[0].center();
-    //     let dijkstra_map = DijkstraMap::new(
-    //         SCREEN_WIDTH, 
-    //         SCREEN_HEIGHT,
-    //         &vec![mb.map.point2d_to_index(mb.player_start)],
-    //         &mb.map,
-    //         1024.0
-    //     );
-    //     const UNREACHABLE : &f32 = &f32::MAX;
-    //     mb.amulet_start = mb.map.index_to_point2d(
-    //     dijkstra_map.map
-    //             .iter()
-    //             .enumerate()
-    //             .filter(|(_, dist)| *dist < UNREACHABLE )
-    //             .max_by(|a,b| a.1.partial_cmp(b.1).unwrap())
-    //             .unwrap().0
-    //     );
-    //     mb
-    // }
+    }
 
     fn fill(&mut self, tile: TileType) {
         self.map.tiles.iter_mut().for_each(|t| *t = tile);
