91780f488a8948c1a435397519044b65f0a8c90d
Daryl Manning <daryl@wakatara.com>
2022-02-06 01:03:36 +0800

    Altered map builder to randomize algos to build dungeon.

--- src/map_builder/mod.rs
+++ src/map_builder/mod.rs
@@ -1,7 +1,9 @@
 use crate::prelude::*;
 const NUM_ROOMS: usize = 20;
-// mod automata;
-// use automata::CellularAutomataArchitect;
+mod rooms;
+use rooms::RoomsArchitect;
+mod automata;
+use automata::CellularAutomataArchitect;
 mod drunkard;
 use drunkard::DrunkardsWalkArchitect;
 
@@ -19,9 +21,13 @@ pub struct MapBuilder {
 
 impl MapBuilder {
     pub fn new(rng: &mut RandomNumberGenerator) -> Self {
-        // let mut architect = CellularAutomataArchitect{};
-        let mut architect = DrunkardsWalkArchitect{};
-        architect.new(rng)
+        let mut architect : Box<dyn MapArchitect> = match rng.range(0, 3) {
+            0 => Box::new(DrunkardsWalkArchitect{}),
+            1 => Box::new(RoomsArchitect{}),
+            _ => Box::new(CellularAutomataArchitect{})
+        };
+        let mut mb = architect.new(rng);
+        mb
     }
 
     fn fill(&mut self, tile: TileType) {
